e54d52de6fc9f7b7d0d1a2ad540b607c55565841
codervim <codervim@gmail.com>
2014-09-30 23:22:57 +0800

    node replace using html

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 $(function() {
     a = enList;
-    alert("jquery ready");
 
-	var n = parseInt( $("#order").text() ) + 1;
-if (1 == n) {
+    var n = parseInt( $("#order").text() ) ;
+    if (1 == n) {
 	$("#article").append( $.get("/en/" + enList[n] ) );	
-}
+	}
     $("#right_arrow").click(function() {
 	var n = parseInt( $("#order").text() ) ;
 	n = n + 1;	
-	$("#article").append( $.get("/en/" + enList[n] ) );	
+	$("#article").html( $.get("/en/" + enList[n] ) );	
 	$("#order").text( n );
     });
 
