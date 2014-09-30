4bd204158a3ccc7827b9ec0faf64498547a13a02
codervim <codervim@gmail.com>
2014-09-30 23:11:15 +0800

    test click

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ $(function() {
     a = enList;
     alert("jquery ready");
     $("#right_arrow").click(function() {
-	var n = $.parseInt( $("#order").text() ) + 1;
+	var n = parseInt( $("#order").text() ) + 1;
 	$("#article").text( $.get("/en/" + enList[n] ) );	
+	$("#order").text( n );
     });
 
 });
