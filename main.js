88779faefb2f8643f931cdf5648fa50f7ad354e0
codervim <codervim@gmail.com>
2014-09-30 23:19:35 +0800

    test click 2

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $(function() {
 
 	var n = parseInt( $("#order").text() ) + 1;
 if (1 == n) {
-	$("#article").insert( $.get("/en/" + enList[n] ) );	
+	$("#article").append( $.get("/en/" + enList[n] ) );	
 }
     $("#right_arrow").click(function() {
 	var n = parseInt( $("#order").text() ) ;
 	n = n + 1;	
-	$("#article").insert( $.get("/en/" + enList[n] ) );	
+	$("#article").append( $.get("/en/" + enList[n] ) );	
 	$("#order").text( n );
     });
 
