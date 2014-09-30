2c99c10aabdfc689e30e36347c237e17cc09bbb1
codervim <codervim@gmail.com>
2014-09-30 23:27:45 +0800

    jquery test

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $(function() {
     $("#right_arrow").click(function() {
 	var n = parseInt( $("#order").text() ) ;
 	n = n + 1;	
-	 var newTxt = $.get("/en/" + enList[n] )
+	 var newTxt = $.get("/en/" + enList[n] ).text();
 alert(newTxt);
 	$("#article").append( newTxt);	
 	$("#order").text( n );
