71d7d0308912e2bb3245f21fabfa2f0db9d5d15e
codervim <codervim@gmail.com>
2014-10-01 10:39:23 +0800

    Update INDEX.js

--- INDEX.js
+++ INDEX.js
@@ -1,11 +1,11 @@
 enList = { 
 1:"what_is_codervim_en", 
-2:"what_is_codervim_en", 
-3:"what_is_codervim_en", 
-4:"what_is_codervim_en", 
-5:"what_is_codervim_en", 
-6:"what_is_codervim_en", 
-7:"what_is_codervim_en", 
+2:"vim_history_present_future", 
+3:"config_styles_EN", 
+4:"vim_lang_EN", 
+5:"keyword_identifier_highlight_EN", 
+6:"difference_vim_emacs_EN", 
+7:"regular_lang_in_VIM_EN", 
  };
 
 
